2e68b096c38db3dd43855b2fe3396ef138cf9925
SerenityTheGame <serenatvarol@gmail.com>
2025-06-03 22:19:01 +0300

    now matching COLUMN names...

--- DAL/AppDBContext.cs
+++ DAL/AppDBContext.cs
@@ -25,45 +25,72 @@ namespace IndieArtMarketplace.DAL
             // Configure User
             modelBuilder.Entity<User>(entity =>
             {
-                entity.HasKey(e => e.UserID);
-                entity.Property(e => e.Username).IsRequired();
-                entity.Property(e => e.Email).IsRequired();
-                entity.Property(e => e.Password).IsRequired();
-                entity.Property(e => e.Role).IsRequired();
+                entity.HasKey(e => e.UserID).HasName("userid");
+                entity.Property(e => e.Username).IsRequired().HasColumnName("username");
+                entity.Property(e => e.Email).IsRequired().HasColumnName("email");
+                entity.Property(e => e.Password).IsRequired().HasColumnName("password");
+                entity.Property(e => e.Role).IsRequired().HasColumnName("role");
+                entity.Property(e => e.RegistrationDate).HasColumnName("registrationdate");
             });
 
             // Configure Artwork
             modelBuilder.Entity<Artwork>(entity =>
             {
-                entity.HasKey(e => e.ArtworkID);
-                entity.Property(e => e.Title).IsRequired();
-                entity.Property(e => e.Description).IsRequired();
-                entity.Property(e => e.Price).HasColumnType("decimal(10,2)");
+                entity.HasKey(e => e.ArtworkID).HasName("artworkid");
+                entity.Property(e => e.Title).IsRequired().HasColumnName("title");
+                entity.Property(e => e.Description).IsRequired().HasColumnName("description");
+                entity.Property(e => e.Price).HasColumnType("decimal(10,2)").IsRequired().HasColumnName("price");
+                entity.Property(e => e.FileURL).IsRequired().HasColumnName("fileurl");
+                entity.Property(e => e.UploadDate).HasColumnName("uploaddate");
+                entity.Property(e => e.ArtistID).IsRequired().HasColumnName("artistid");
+                entity.Property(e => e.License).HasColumnName("license");
                 entity.HasOne<User>()
                     .WithMany()
-                    .HasForeignKey(e => e.ArtistID);
+                    .HasForeignKey(e => e.ArtistID)
+                    .HasConstraintName("fk_artworks_users_artistid");
             });
 
             // Configure MusicTrack
             modelBuilder.Entity<MusicTrack>(entity =>
             {
-                entity.HasKey(e => e.TrackID);
-                entity.Property(e => e.Title).IsRequired();
-                entity.Property(e => e.Description).IsRequired();
-                entity.Property(e => e.Price).HasColumnType("decimal(10,2)");
+                entity.HasKey(e => e.TrackID).HasName("trackid");
+                entity.Property(e => e.Title).IsRequired().HasColumnName("title");
+                entity.Property(e => e.Description).IsRequired().HasColumnName("description");
+                entity.Property(e => e.Price).HasColumnType("decimal(10,2)").IsRequired().HasColumnName("price");
+                entity.Property(e => e.FileURL).IsRequired().HasColumnName("fileurl");
+                entity.Property(e => e.UploadDate).HasColumnName("uploaddate");
+                entity.Property(e => e.ArtistID).IsRequired().HasColumnName("artistid");
+                entity.Property(e => e.License).HasColumnName("license");
                 entity.HasOne<User>()
                     .WithMany()
-                    .HasForeignKey(e => e.ArtistID);
+                    .HasForeignKey(e => e.ArtistID)
+                    .HasConstraintName("fk_musictracks_users_artistid");
             });
 
             // Configure Transaction
             modelBuilder.Entity<Transaction>(entity =>
             {
-                entity.HasKey(e => e.TransactionID);
-                entity.Property(e => e.Amount).HasColumnType("decimal(10,2)");
+                entity.HasKey(e => e.TransactionID).HasName("transactionid");
+                entity.Property(e => e.BuyerID).IsRequired().HasColumnName("buyerid");
+                entity.Property(e => e.ArtworkID).HasColumnName("artworkid");
+                entity.Property(e => e.TrackID).HasColumnName("trackid");
+                entity.Property(e => e.PurchaseDate).HasColumnName("purchasedate");
+                entity.Property(e => e.Amount).HasColumnType("decimal(10,2)").IsRequired().HasColumnName("amount");
+
                 entity.HasOne<User>()
                     .WithMany()
-                    .HasForeignKey(e => e.BuyerID);
+                    .HasForeignKey(e => e.BuyerID)
+                    .HasConstraintName("fk_transactions_users_buyerid");
+
+                entity.HasOne<Artwork>()
+                    .WithMany()
+                    .HasForeignKey(e => e.ArtworkID)
+                    .HasConstraintName("fk_transactions_artworks_artworkid");
+
+                entity.HasOne<MusicTrack>()
+                    .WithMany()
+                    .HasForeignKey(e => e.TrackID)
+                    .HasConstraintName("fk_transactions_musictracks_trackid");
             });
         }
     }
