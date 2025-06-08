14bd4050602c0fb2de9aa7b599c5adc8ec04800b
SerenityTheGame <serenatvarol@gmail.com>
2025-06-09 01:22:08 +0300

    erasing imagekit stuff from program.cs

--- Program.cs
+++ Program.cs
@@ -14,20 +14,11 @@ using Microsoft.AspNetCore.Http.Features;
 using System.IO;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
-using ImageKit;
 using IndieArtMarketplace.Models;
 
 // Adding a dummy comment to force a new deploy on Render.com
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Configure ImageKit settings
-builder.Services.Configure<ImageKitSettings>(builder.Configuration.GetSection("ImageKit"));
-builder.Services.AddSingleton(sp =>
-{
-    var imageKitSettings = sp.GetRequiredService<Microsoft.Extensions.Options.IOptions<ImageKitSettings>>().Value;
-    return new ImageKitClient(imageKitSettings.ImageKitPublicKey, imageKitSettings.ImageKitPrivateKey, imageKitSettings.ImageKitUrlEndpoint);
-});
 
 // Configure logging
 builder.Logging.ClearProviders();
@@ -77,6 +68,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 // Register Services
 builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<IBlobStorageService, BlobStorageService>();
 
 // Add session support
 builder.Services.AddSession(options =>
